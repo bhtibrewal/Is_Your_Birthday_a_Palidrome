fa8018ce1e404d7b700d238ec628935cac3680f6
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-10-30 18:16:48 +0530

    check-palindrome done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,59 @@ import "./styles.css";
 import { useState } from "react";
 
 var date = "";
+
 export default function App() {
-  const [output, setOutput] = useState("");
+  //const [output, setOutput] = useState("");
   function clickHandler() {
-    console.log(date);
+    if (checkAllDateformats());
+    else console.log(getNextDate());
   }
 
+  function getNextDate() {
+    var numDateArray = getNumDateArray();
+
+    return numDateArray;
+  }
+  function getNumDateArray() {
+    var dateArray = date.split("-"); //["yyyy","mm","dd"]
+    var d = Number(dateArray[2]);
+    var m = Number(dateArray[1]);
+    var y = Number(dateArray[0]);
+    return [d, m, y];
+  }
+  function checkAllDateformats() {
+    var allDateFormats = getAllDateFormats();
+    var palindrome = false;
+    for (var i = 0; i < allDateFormats.length; i++) {
+      if (isPalindrome(allDateFormats[i])) {
+        palindrome = true;
+        console.log(allDateFormats[i]);
+        break;
+      }
+    }
+    return palindrome;
+  }
+  function getAllDateFormats() {
+    var dateArray = date.split("-"); //["yyyy","mm","dd"]
+
+    var ddmmyyyy = dateArray[2] + dateArray[1] + dateArray[0];
+    var mmddyyyy = dateArray[1] + dateArray[2] + dateArray[0];
+    var yyyymmdd = dateArray[1] + dateArray[2] + dateArray[0];
+    var ddmmyy = dateArray[2] + dateArray[1] + dateArray[0].slice(-2);
+    var mmddyy = dateArray[1] + dateArray[2] + dateArray[0].slice(-2);
+    var yymmdd = dateArray[0].slice(-2) + dateArray[1] + dateArray[2];
+
+    var allDateFormats = [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
+    // console.log(dateArray);
+    //console.log("date:", allDateFormats);
+    return allDateFormats;
+  }
+  function isPalindrome(str) {
+    return str === reverseStr(str);
+  }
+  function reverseStr(str) {
+    return str.split("").reverse().join("");
+  }
   return (
     <div className="App section">
       <h1>Palindrome Birthday!</h1>
@@ -22,6 +69,7 @@ export default function App() {
       <button type="submit" onClick={clickHandler}>
         Check
       </button>
+      <h2></h2>
     </div>
   );
 }
